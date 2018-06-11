614e6dbecfd210a757c2db35176d71908939afe6
Venczel Zsombor <venczel.zsombor@gmail.com>
2018-06-11 22:14:56 +0200

    first commit

---
+++ init.el
@@ -0,0 +1,87 @@
+; start package.el with emacs
+(require 'package)
+; add MELPA to repository list
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+; initialize package.el
+(package-initialize)
+; start auto-complete with emacs
+(require 'auto-complete)
+; do default config for auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
+; start yasnippet with emacs
+(require 'yasnippet)
+(yas-global-mode 1)
+; let's define a function which initializes auto-complete-c-headers and gets called for c/c++ hooks
+
+; these pathes are obtained by the command
+; $ gcc -xc++ -v -
+(defun my:ac-c-header-init ()
+  (require 'auto-complete-c-headers)
+  (add-to-list 'ac-sources 'ac-source-c-headers)
+  (add-to-list 'achead:include-directories '"/usr/include/c++/6")
+  (add-to-list 'achead:include-directories '"/usr/include/x86_64-linux-gnu/c++/6")
+  (add-to-list 'achead:include-directories '"/usr/include/c++/6/backward")
+  (add-to-list 'achead:include-directories '"/usr/lib/gcc/x86_64-linux-gnu/6/include")
+  (add-to-list 'achead:include-directories '"/usr/local/include")
+  (add-to-list 'achead:include-directories '"/usr/lib/gcc/x86_64-linux-gnu/6/include-fixed")
+  (add-to-list 'achead:include-directories '"/usr/include/x86_64-linux-gnu")
+  (add-to-list 'achead:include-directories '"/usr/include")
+  )
+; now let's call this function from c/c++ hooks
+(add-hook 'c++-mode-hook 'my:ac-c-header-init)
+(add-hook 'c-mode-hook 'my:ac-c-header-init)
+
+; Fix iedit bug in Mac
+(define-key global-map (kbd "C-c i") 'iedit-mode)
+;; 					; turn on Semantic
+;; (semantic-mode 1)
+;; 					; let's define a function which adds semantic as a suggestion backend to auto complete
+;; 					; and hook this function to c-mode-common-hook
+;; (defun my:add-semantic-to-autocomplete()
+;;   (add-to-list 'ac-sources 'ac-source-semantic)
+;;   )
+;; (add-hook 'c-mode-common-hook 'my:add-semantic-to-autocomplete)
+;; 					; turn on ede mode
+;; (global-ede-mode 1)
+;; 					; create a project for our program.
+;; (ede-cpp-root-project "my project" :file "~/demos/my_program/src/main.cpp"
+;; 		      :include-path '("/../my_inc"))
+;; 					; you can use system-include-path for setting up the system header file locations.
+;; 					; turn on automatic reparsing of open buffers in semantic
+;; (global-semantic-idle-scheduler-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-enabled-themes (quote (tsdh-dark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(setq ring-bell-function 'ignore)
+
+;; Run SageMath by M-x run-sage instead of M-x sage-shell:run-sage
+(sage-shell:define-alias)
+
+;; Turn on eldoc-mode in Sage terminal and in Sage source files
+(add-hook 'sage-shell-mode-hook #'eldoc-mode)
+(add-hook 'sage-shell:sage-mode-hook #'eldoc-mode)
+(put 'dired-find-alternate-file 'disabled nil)
+
+;;; Modify the backup directory to not to flood the working directory
+;;; with *~ backup files
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+      backup-by-copying t    ; Don't delink hardlinks
+      version-control t      ; Use version numbers on backups
+      delete-old-versions t  ; Automatically delete excess backups
+      kept-new-versions 20   ; how many of the newest versions to keep
+      kept-old-versions 5    ; and how many of the old
+      )
