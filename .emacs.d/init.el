367ed6323a1fbeec1dfa6eea3f3eb09e94ff5053
Venczel Zsombor <venczel.zsombor@gmail.com>
2019-01-06 15:56:46 +0100

    Configured for securely contacting package archives

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,101 @@
 ;;; ~/emacs.d/init.el
 
+(setq init-dir "~/.emacs.d/")
+(setq custom-file (concat init-dir "custom.el"))
+(load custom-file :noerror)
+
+;;; Secure Emacs
+(require 'cl)
+(setq tls-checktrust t)
+(setq python (executable-find "python"))
+
+;;; Setup TLS certification
+(let ((trustfile
+       (replace-regexp-in-string
+	"\\\\" "/"
+	(replace-regexp-in-string
+	 "\n" ""
+	 (shell-command-to-string (concat python " -m certifi"))))))
+  (setq tls-program
+	(list
+	 (format "gnutls-cli%s --x509cafile %s -p %%p %%h"
+		 (if (eq window-system 'w32) ".exe" "") trustfile)))
+  (setq gnutls-verify-error t)
+  (setq gnutls-trustfiles (list trustfile)))
+
+;;; Test whether TLS is configured well
+;; (let ((bad-hosts
+;;        (loop for bad
+;; 	     in `("https://wrong.host.badssl.com/"
+;; 		  "https://self-signed.badssl.com/")
+;; 	     if (condition-case e
+;; 		    (url-retrieve
+;; 		     bad (lambda (retrieved) t))
+;; 		  (error nil))
+;; 	     collect bad)))
+;;   (if bad-hosts
+;;       (error (format "tls misconfigured; retrieved %s ok"
+;; 		     bad-hosts))
+;;     (url-retrieve "https://badssl.com"
+;; 		  (lambda (retrieved) t))))
+
+;;; Setting up MELPA ; See in custom-set-vars in custom.el
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+(defvar gnu '("gnu" . "https://elpa.gnu.org/packages/"))
+(defvar melpa '("melpa" . "https://melpa.org/packages/"))
+(defvar melpa-stable '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(defvar org-elpa '("org" . "http://orgmode.org/elpa/"))
+
+;;; Add marmalade to package repos
+(setq package-archives nil)
+(add-to-list 'package-archives melpa-stable t)
+(add-to-list 'package-archives melpa t)
+(add-to-list 'package-archives gnu t)
+(add-to-list 'package-archives org-elpa t)
+
 (package-initialize)
 
+(unless (and (file-exists-p (concat init-dir "elpa/archives/gnu"))
+             (file-exists-p (concat init-dir "elpa/archives/melpa"))
+             (file-exists-p (concat init-dir "elpa/archives/melpa-stable")))
+  (package-refresh-contents))
+
+(defun packages-install (&rest packages)
+  (message "running packages-install")
+  (mapc (lambda (package)
+          (let ((name (car package))
+                (repo (cdr package)))
+            (when (not (package-installed-p name))
+              (let ((package-archives (list repo)))
+                (package-initialize)
+                (package-install name)))))
+        packages)
+  (package-initialize)
+  (delete-other-windows))
+
+;; Install extensions if they're missing
+(defun init--install-packages ()
+  (message "Lets install some packages")
+  (packages-install
+   ;; Since use-package this is the only entry here
+   ;; ALWAYS try to use use-package!
+   (cons 'use-package melpa)
+   ))
+
+(condition-case nil
+    (init--install-packages)
+  (error
+   (package-refresh-contents)
+   (init--install-packages)))
+
+(use-package diminish
+	     :ensure t)
+
+;; Just to move fast
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;;; AC & Yas
 (require 'auto-complete)
 (require 'auto-complete-config)
 (ac-config-default)
@@ -11,33 +103,16 @@
 (require 'yasnippet)
 (yas-global-mode 1)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
- '(custom-enabled-themes (quote (misterioso)))
- '(org-agenda-files
-   (quote
-    ("~/.emacs.d/org/org-config.el" "~/org/email.org" "~/org/munka.org" "~/org/sajat.org" "~/org/suli.org" "~/org/tanulas.org" "~/Dokumentumok/EGYETEM/TDK/org/tdk.org"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;;; Global key bindings
+(define-key global-map (kbd "M-p") 'cua-mode)
 (define-key global-map (kbd "C-c i") 'iedit-mode)
 (global-set-key (kbd "C-c r") 'rectangle-mark-mode)
 (global-set-key (kbd "<f5>") (lambda() (interactive)(shell)))
-(global-set-key (kbd "<f6>") (lambda() (interactive)(find-file "~/.emacs.d/init.el")))
+(global-set-key (kbd "<f6>") (lambda() (interactive)
+			       (find-file "~/.emacs.d/init.el")))
 (global-set-key (kbd "<f7>") (lambda() (interactive)(org-agenda-list)))
-;; (global-set-key (kbd "<f9>") (lambda() (interactive)(run-sage RET))) ; TODO kbd for run-sage
+;;; TODO kbd for run-sage
+;; (global-set-key (kbd "<f9>") (lambda() (interactive)(run-sage RET)))
 
 ;;; Set prog-mode hooks
 (require 'rainbow-delimiters)
